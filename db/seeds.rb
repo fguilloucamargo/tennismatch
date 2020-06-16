d7d92869eec0910fd4f406cd5bf48b4d8eda504f
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-16 11:44:30 +0200

    User instance creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,11 @@
 require 'open-uri'
 require 'nokogiri'
 
+User.destroy_all
+
 url = "https://www.eurosport.fr/tennis/atp/standing.shtml"
+
+classement = ["NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30", "15/5", "15/4", "15/3", "15/2", "15/1", "15", "5/6", "4/6", "3/6", "2/6", "1/6", "0", "-2/6", "-4/6", "-15", "-30"]
 
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
@@ -17,10 +21,10 @@ result = []
 # photo = search('.player img')[0].attributes["data-isg-lazy"].value
 # country = search('.country img')[0].attributes["title"].value
 
-html_doc.search('.standing-table__player-link')[0..5].each do |element|
+html_doc.search('.standing-table__player-link')[0..2].each do |element|
   result << [element.search('.text').children.text, element.search('.player img')[0].attributes["data-isg-lazy"].value, element.search('.country img')[0].attributes["title"].value ]
 end
 
 result.each do |player|
-
+  User.create!(first_name: player[0], photo: player[1], address: player[2], gender: "male", email: "#{player[0].downcase.gsub(" ", "_")}@gmail.com", password: "secret", level: classement.shuffle.first)
 end

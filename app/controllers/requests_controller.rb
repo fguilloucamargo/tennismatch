d41a3edbaab54b4c13f46cc91321686d2518847e
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-16 10:39:58 +0200

    creation seed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+
   def index
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'nokogiri'
+
+url = "https://www.eurosport.fr/tennis/atp/standing.shtml"
+
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+result = []
+# photo = search('.player img')[0].attributes["data-isg-lazy"].value
+# country = search('.country img')[0].attributes["title"].value
+
+html_doc.search('.standing-table__player-link')[0..5].each do |element|
+  result << [element.search('.text').children.text, element.search('.player img')[0].attributes["data-isg-lazy"].value, element.search('.country img')[0].attributes["title"].value ]
+end
+
+p result

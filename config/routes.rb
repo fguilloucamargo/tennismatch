2d5b4618f623ef78669b6e307baf0df82b376397
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-16 11:53:20 +0200

    Change routes.rb and remove Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'matchs/index'
-  get 'requests/index'
-  get 'requests/show'
-  get 'requests/new'
   devise_for :users
   root to: 'requests#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

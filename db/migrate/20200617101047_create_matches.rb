2b144422fbe79438dfe123fcd187657ee89802c6
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-17 12:11:58 +0200

    Change references

--- db/migrate/20200617101047_create_matches.rb
+++ db/migrate/20200617101047_create_matches.rb
@@ -1,8 +1,8 @@
 class CreateMatches < ActiveRecord::Migration[6.0]
   def change
     create_table :matches do |t|
-      t.references :requests, null: false, foreign_key: true
-      t.references :users, null: false, foreign_key: true
+      t.references :request, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end

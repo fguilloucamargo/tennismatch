50b2dbf62f61bdc61bf18201ccba3919c2539d3c
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-17 15:07:23 +0200

    Match controller and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :requests
-  has_many :matchs
+  has_many :matchs, dependent: :destroy
 
   validates :first_name, :level, :address, :gender, presence: true
   LEVEL = ["NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30", "15/5", "15/4", "15/3", "15/2", "15/1", "15", "5/6", "4/6", "3/6", "2/6", "1/6", "0", "-2/6", "-4/6", "-15", "-30"]

513f2f18605bb7e7365fc42e7140b3d19ed18462
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-19 06:41:54 +0200

    Add avatar par default

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'devise'
 gem 'cloudinary', '~> 1.12.0'
 gem 'dotenv-rails', groups: [:development, :test]
 
+
 gem 'pg_search', '~> 2.3.0'
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,13 @@ class User < ApplicationRecord
 
   validates :first_name, :level, :address, :gender, presence: true
   LEVEL = ["NC", "40", "30/5", "30/4", "30/3", "30/2", "30/1", "30", "15/5", "15/4", "15/3", "15/2", "15/1", "15", "5/6", "4/6", "3/6", "2/6", "1/6", "0", "-2/6", "-4/6", "-15", "-30"]
-  GENDER = ["Men", "Women"]
+  GENDER = ["Man", "Woman"]
   validates :level, inclusion: { in: LEVEL }
   validates :gender, inclusion: { in: GENDER }
+
+  after_validation :set_photo
+
+  def set_photo
+    self.photo = "https://res.cloudinary.com/dhtapshlv/image/upload/b_rgb:ffffff/v1592540966/tennis-player_mdhkjh.svg"
+  end
 end

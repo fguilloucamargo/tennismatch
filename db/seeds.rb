4e1122c8c48a178da5b7378514186a9d9b16d47b
Romain Gericot <romain.gericot@gmail.com>
2020-06-19 11:37:53 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,19 @@ roger_desc = "Jeu : Droitier, revers à une main. Roger Federer possède une tec
 stan_desc = "Jeu : Droitier, revers à une main, Stanislas Wawrinka est un joueur puissant qui développe un jeu d'attaque plutôt fluide et en variations, exploitant une très bonne technique grâce à une impeccable mobilité, un excellent timing, et un grand sens du jeu. Wawrinka est un joueur qui aime dicter le jeu pour marquer son empreinte sur le match et ainsi mettre la pression à son adversaire grâce à ses lourdes frappes du fond de court. Il est également doté d'une grande endurance physique, ce qui a mené Roger Federer à le surnommer « Stanimal », surnom repris par les médias et les joueurs français."
 stefanos_desc = "Jeu : Droitier, revers à une main, Le Grec est avant tout un attaquant, et il aime gagner ses points pas des coups gagnants. Son coup droit est une arme de très bonne qualité. Revers: Petit bijou d'esthétisme, son revers est également terriblement efficace."
 
-novak = User.create!(first_name: "Novak", last_name: "Djokovic", address: "Serbie", gender: "Men", email: "novak@atp.com", password: "secret", level: classement.shuffle.first, description: novak_desc, photo: "https://res.cloudinary.com/dhtapshlv/image/upload/v1592421797/Novak_Djokovic_64_srhdni.jpg")
-nadal = User.create!(first_name: "Rafael", last_name: "Nadal", address: "Espagne", gender: "Men", email: "rafael@atp.com", password: "secret", level: classement.shuffle.first, description: nadal_desc,  photo: "https://res.cloudinary.com/dhtapshlv/image/upload/v1592421832/Rafael_Nadal_100_rauek2.jpg")
-dominic = User.create!(first_name: "Dominic", last_name: "Thiem", address: "Autriche", gender: "Men", email: "dominic@atp.com", password: "secret", level: classement.shuffle.first, description: dominic_desc, photo: "https://res.cloudinary.com/dhtapshlv/image/upload/v1592421861/Dominic_Thiem_85_w3icfr.jpg")
-federer = User.create!(first_name: "Roger", last_name: "Federer", address: "Suisse", gender: "Men", email: "roger@atp.com", password: "secret", level: classement.shuffle.first, description: roger_desc, photo: "https://res.cloudinary.com/dhtapshlv/image/upload/v1592421896/Roger_Federer_73_uqnvux.jpg")
-stan = User.create!(first_name: "Stan", last_name: "Wawrinka", address: "Suisse", gender: "Men", email: "stan@atp.com", password: "secret", level: classement.shuffle.first, description: stan_desc, photo: "https://res.cloudinary.com/dlwpvvdub/image/upload/c_fill,g_auto:face,h_250,w_250/v1592474095/Stanislas-Wawrinka_mg9jna.jpg")
-stefanos = User.create!(first_name: "Stefanos", last_name: "Tsitsipas", address: "Grece", gender: "Men", email: "stefanos@atp.com", password: "secret", level: classement.shuffle.first, description: stefanos_desc, photo: "https://res.cloudinary.com/dlwpvvdub/image/upload/c_fill,g_face,h_200,q_auto:best,w_200/v1592486076/file73qv0qddg1ubuwk1ayv_pcswf4.jpg")
+novak = User.create!(first_name: "Novak", last_name: "Djokovic", address: "Serbie", gender: "Man", email: "novak@atp.com", password: "secret", level: classement.shuffle.first, description: novak_desc, photo: "https://res.cloudinary.com/dzghetxbv/image/upload/v1592559223/dsusl83ac61ewjxkltde_sjdlqh.jpg")
+nadal = User.create!(first_name: "Rafael", last_name: "Nadal", address: "Espagne", gender: "Man", email: "rafael@atp.com", password: "secret", level: classement.shuffle.first, description: nadal_desc,  photo: "https://res.cloudinary.com/dzghetxbv/image/upload/v1592559223/Rafael_Nadal_100_rauek2_sidisc.jpg")
+dominic = User.create!(first_name: "Dominic", last_name: "Thiem", address: "Autriche", gender: "Man", email: "dominic@atp.com", password: "secret", level: classement.shuffle.first, description: dominic_desc, photo: "https://res.cloudinary.com/dzghetxbv/image/upload/v1592559223/Dominic_Thiem_85_w3icfr_omphwb.jpg")
+federer = User.create!(first_name: "Roger", last_name: "Federer", address: "Suisse", gender: "Man", email: "roger@atp.com", password: "secret", level: classement.shuffle.first, description: roger_desc, photo: "https://res.cloudinary.com/dzghetxbv/image/upload/v1592559223/Roger_Federer_73_uqnvux_iyckw0.jpg")
+# stan = User.create!(first_name: "Stan", last_name: "Wawrinka", address: "Suisse", gender: "Men", email: "stan@atp.com", password: "secret", level: classement.shuffle.first, description: stan_desc, photo: "https://res.cloudinary.com/dlwpvvdub/image/upload/c_fill,g_auto:face,h_250,w_250/v1592474095/Stanislas-Wawrinka_mg9jna.jpg")
+# stefanos = User.create!(first_name: "Stefanos", last_name: "Tsitsipas", address: "Grece", gender: "Men", email: "stefanos@atp.com", password: "secret", level: classement.shuffle.first, description: stefanos_desc, photo: "https://res.cloudinary.com/dlwpvvdub/image/upload/c_fill,g_face,h_200,q_auto:best,w_200/v1592486076/file73qv0qddg1ubuwk1ayv_pcswf4.jpg")
 
 times = ["10h", "18h30", "20h", "21h", "14h30", "17h", "15h30"]
 cities = ["Paris", "London", "Prague", "Barcelone", "Madrid", "Lisbon", "Budapest", "Amsterdam", "Tokyo", "Rome", "Mexico"]
 
 25.times do
   request = Request.new
-  request.user = [novak, nadal, federer, dominic, stan, stefanos].shuffle.first
+  request.user = [novak, nadal, federer, dominic].shuffle.first
   request.date = Date.today+rand(100)
   request.time = times.shuffle.first
   request.location = cities.shuffle.first
@@ -44,6 +44,5 @@ m2 = Match.new
 m2.request = Request.last
 m2.user = User.last
 m2.save
->>>>>>> f8b9c6d0ba607e02520f9e4849e3545489d76d47
 
 
